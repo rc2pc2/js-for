5865448830cc6c127b415e444a7dc215073422bd
rc2pc2 <petriccarcc@gmail.com>
2023-05-16 13:16:19 +0200

    for loop initial test

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,16 @@
 
 
+const ulElement = document.querySelector('ul');
+
+for ( let index = 0 ; index < 10 ; index = index + 1){
+    const liElement = document.createElement('li');
+    liElement.innerHTML = index;
+
+    ulElement.appendChild(liElement);
+}
+
+
+
 
 
 
